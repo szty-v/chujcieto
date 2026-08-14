dde07f90e1af53acf4521339a49e8ef294d2e57b
szty-v <lookgrit1@gmail.com>
2026-08-14 15:58:04 +0200

    Update BLACKSIGIL_trait_banner_native_hotbar_v15_persistent_rejoin.lua

--- BLACKSIGIL_trait_banner_native_hotbar_v15_persistent_rejoin.lua
+++ BLACKSIGIL_trait_banner_native_hotbar_v15_persistent_rejoin.lua
@@ -38,7 +38,7 @@ local StarterPlayer = game:GetService("StarterPlayer")
 local LocalPlayer = Players.LocalPlayer
 
 -- Set this to the raw URL hosting this BLACKSIGIL build for rejoin auto-execution.
-local BLACKSIGIL_RAW_URL = "PASTE_RAW_FILE_URL_HERE"
+local BLACKSIGIL_RAW_URL = "https://github.com/szty-v/chujcieto/raw/refs/heads/main/BLACKSIGIL_trait_banner_native_hotbar_v15_persistent_rejoin.luaE"
 local PlayerGui = LocalPlayer:WaitForChild("PlayerGui")
 
 -- Restore normal BLACKSIGIL rejoin after execution/rejoin.
